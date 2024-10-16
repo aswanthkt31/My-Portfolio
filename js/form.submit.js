560d88743e849bbe871eed25d586741cbfdbe7bf
aswanthkt31 <aswanthkt99@gmail.com>
2024-10-16 18:52:51 +0530

    first cmt

---
+++ js/form.submit.js
@@ -0,0 +1,14 @@
+
+// <!-- --------------------Submision------------------ -->
+  
+//   <script>
+// 	const scriptURL = 'https://script.google.com/macros/s/AKfycbxnHAZr-pPtuIuvojboq-_HRieCxWmKVuKpuKVDTXnQfOnN85g3ZiRdtAEoWd_TPz8n/exec'
+// 	const form = document.forms['submit-to-google-sheet']
+  
+// 	form.addEventListener('submit', e => {
+// 	  e.preventDefault()
+// 	  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+// 		.then(response => console.log('Success!', response))
+// 		.catch(error => console.error('Error!', error.message))
+// 	})
+//   </script>
